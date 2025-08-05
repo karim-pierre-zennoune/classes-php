4f0bbbba6a4006ef418b5b712b49bfd0601fe647
Karim Pierre ZENNOUNE <karim-pierre.zennoune@laplateforme.io>
2025-08-05 11:06:21 +0200

    etape 1 classes avec mysqli DONE + main de test

--- User.php
+++ User.php
@@ -8,6 +8,7 @@ class User
     public $firstname;
     public $lastname;
     private $mysqli;
+    private $online = false;
 
     public function __construct()
     {
@@ -27,10 +28,11 @@ class User
                 if (mysqli_stmt_execute($stmt)) {
                     mysqli_stmt_store_result($stmt);
                     if (mysqli_stmt_num_rows($stmt) == 1) {
-                        return null;
+                        throw new Exception('login already exists');
+                        // return null;
                     }
                 } else {
-                    echo "Oops! Something went wrong. Please try again later.";
+                    throw new Exception('Something went wrong');
                 }
                 mysqli_stmt_close($stmt);
             }
@@ -58,18 +60,14 @@ class User
                 } else {
                     return null;
                 }
+            } else {
+                throw new Exception('Something went wrong');
             }
         }
 
     }
 
 
-    //     Connecte l’utilisateur, et
-// donne aux attributs de
-// la classe les valeurs
-// correspondantes à
-// celles de l’utilisateur
-// connecté.
     public function connect($login, $password)
     {
         $sql = "SELECT id, login, password, email, firstname, lastname FROM utilisateurs WHERE login = ?";
@@ -82,27 +80,109 @@ class User
                     if (mysqli_stmt_fetch($stmt)) {
                         if (password_verify($password, $ret_password)) {
                             $this->id = $ret_id;
+                            $this->online = true;
                             $this->login = $ret_login;
                             $this->email = $ret_email;
                             $this->firstname = $ret_firstname;
                             $this->lastname = $ret_lastname;
+                        } else {
+                            throw new Exception('Incorrect password');
                         }
                     }
+                } else {
+                    throw new Exception('User not found');
                 }
+            } else {
+                throw new Exception('Unable to connect');
             }
             mysqli_stmt_close($stmt);
+        } else {
+            throw new Exception('Unable to connect');
         }
     }
 
 
     public function disconnect()
     {
-        $this->id = null;
+        $this->online = false;
     }
 
     public function delete()
     {
+        $sql = "DELETE FROM utilisateurs WHERE id = ?";
+        if ($stmt = mysqli_prepare($this->mysqli, $sql)) {
+            mysqli_stmt_bind_param($stmt, "s", $this->id);
+            if (!mysqli_stmt_execute($stmt)) {
+                throw new Exception('Unable to execute DELETE instruction');
+            }
+            $this->online = false;
+            mysqli_stmt_close($stmt);
+        } else {
+            throw new Exception('Unable to execute DELETE instruction');
+        }
+    }
 
+    public function update($login, $password, $email, $firstname, $lastname)
+    {
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "UPDATE utilisateurs SET login = ?, password = ?, email = ?, firstname = ?, lastname = ? WHERE id = ?";
+        if ($stmt = mysqli_prepare($this->mysqli, $sql)) {
+            mysqli_stmt_bind_param($stmt, "ssssss", $login, $param_password, $email, $firstname, $lastname, $this->login);
+            if (!mysqli_stmt_execute($stmt)) {
+                throw new Exception('Could not update user');
+            }
+            mysqli_stmt_close($stmt);
+        } else {
+            throw new Exception('Could not update user');
+        }
+
+        $this->login = $login;
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+
+    public function isConnected()
+    {
+        return $this->online;
+    }
+
+    public function getAllInfos()
+    {
+        return [
+            "id" => $this->id,
+            "login" => $this->login,
+            "email" => $this->email,
+            "firstname" => $this->firstname,
+            "lastname" => $this->lastname,
+        ];
+    }
+
+
+
+
+
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function getLastname()
+    {
+        return $this->lastname;
     }
 }
 
